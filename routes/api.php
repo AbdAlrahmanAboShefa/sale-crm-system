68c0fb3953f0bd8625a5cfe07ae4579da6d26fdf
AbdAlRhmanAboShefa <abwdt4545@gmail.com>
2025-12-12 16:44:36 +0300

    create post crud with resourses

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+use \App\Http\Controllers\api\v1\PostController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+Route::get('/hello', function () {
+    return ['message' => 'Hello, World!'];
+});
+Route::prefix('v1' )->group(function () {
+    Route::apiResource('posts', PostController::class);
+});
